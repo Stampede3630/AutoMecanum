29d4a12f05506417d9a911e632f55731e9a4364b
StampedeRobotics <breckstampede@gmail.com>
2018-05-25 15:22:44 -0500

    Added additional vector class

--- src/org/usfirst/frc/team3630/robot/DriveTrain.java
+++ src/org/usfirst/frc/team3630/robot/DriveTrain.java
@@ -1,9 +1,11 @@
 package org.usfirst.frc.team3630.robot;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 
-//These imports are necissary for this specific implementation of the code, but not more generic versions.
+//These imports are necessary for this specific implementation of the code, but not more generic versions.
 import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.XboxController;
 
 
 /*
@@ -12,20 +14,49 @@ import edu.wpi.first.wpilibj.Talon;
  */
 public class DriveTrain {
 	
+
+	
 	//These are declared generically so that any speed controller can be used. This line does not need to be modified, but you can define your specific type of speed controller in the constructor (ie Talon, Victor etc)
 	SpeedController fL, fR, rL, rR;
 	MecanumDrive driveTrain;
+	XboxController xBox;
 	public DriveTrain () {
+		
+		xBox = new XboxController(0);
 		
 		//Constructions can change here
 		fL = new Talon(1);
 		fR = new Talon(2);
 		rL = new Talon(3);
 		rR = new Talon(4);
+		
+		
 		driveTrain = new MecanumDrive(fL,rL,fR,rR);
 	}
 	
+	public void teleopDrive () {
+		driveTrain.driveCartesian(
+				xBox.getX(Hand.kLeft),
+				xBox.getY(Hand.kRight), 
+				xBox.getX(Hand.kRight), 
+				0);
+	}
 	
 
-	
+	public Vector forwardMecanum(double frontLeft, double rearLeft, double frontRight, double rearRight) {
+		double vX = (frontLeft + rearLeft + frontRight + rearRight) / 4;
+		double vY = (-frontLeft + rearLeft - rearRight + frontRight) / 4;
+		double vTheta = (-frontLeft - rearLeft + rearRight + frontRight) / (4 * 90); //I believe this is in degrees
+		
+		return new Vector (vX,vY,vTheta);
+	}
+
+	class Vector {
+		public double x,y,theta;
+		public Vector (double _x, double _y, double _theta){
+			x=_x;
+			y=_y;
+			theta = _theta;
+		}
+	}
 }
